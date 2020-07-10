3cf15c80018b7485391c5e2dce494b724ba53d27
leon-s1 <chuprov.sa@gmail.com>
2020-07-10 16:15:36 +0700

    change file modal.js

--- js-plagins-practice/index.js
+++ js-plagins-practice/index.js
@@ -1,3 +1,10 @@
+const fruits = [
+  {id: 1, title: 'Манго', price: 20, img: 'https://e0.edimdoma.ru/data/ingredients/0000/1089/1089-ed4_small.jpg?1482770262'},
+  {id: 2, title: 'Апельсины', price: 30, img: 'https://e3.edimdoma.ru/data/ingredients/0000/8813/8813-ed4_small.jpg?1482763602'},
+  {id: 3, title: 'Гранат', price: 40, img: 'https://e1.edimdoma.ru/data/ingredients/0000/8722/8722-ed4_small.jpg?1482763618'}
+]
+
+
 const modal = $.modal({
   title: 'Vladilen Modal',
   closable: true,
@@ -9,9 +16,11 @@ const modal = $.modal({
   footerButtons: [
   {text: 'Ok', type: 'primary', handler() {
     console.log('Primary btn clicked')
+    modal.close()
   }},
   {text: 'Cancel', type: 'danger', handler() {
     console.log('Danger btn clicked')
+    modal.close()
   }}
   ]
 })
