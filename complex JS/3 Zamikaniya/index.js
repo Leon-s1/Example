775b6bcc7066b8cbc3b356b97e600e9126019176
Leon-s1 <chuprov.sa@gmail.com>
2020-07-14 20:48:05 +0700

    changed index.js zamikaniya

--- complex JS/3 Zamikaniya/index.js
+++ complex JS/3 Zamikaniya/index.js
@@ -1,52 +1,37 @@
-function hello() {
-  console.log('Hello', this);
-}
-
-const person = {
-  name: 'Vladilen',
-  age: 25,
-  sayHello: hello,
-  sayHelloWindow: hello.bind(window),
-  logInfo: function(job, phone) {
-    console.group(`${this.name} info`) //группировка объекта
-    console.log(`Name is ${this.name}`);
-    console.log(`Age is ${this.age}`);
-    console.log(`Job is ${job}`);
-    console.log(`Phone is ${phone}`);
-    console.groupEnd()
+function createCalcFunction(n) {
+  return function() {
+    console.log(1000*n);
   }
 }
-const lena = {
-  name: 'Elena',
-  age: 23
-}
-// person.logInfo('Frontend', '8-999-123-12-22')
-//передача параметров с помощью метода bind
-//1 СПОСОБ****************************************
-// person.logInfo.bind(lena)('Frontend', '8-999-123-12-22')
-const fnLenaInfoLog = person.logInfo.bind(lena, 'Frontend', '8-999-123-12-22') //можно передавать в метод bind через запятую
-// fnLenaInfoLog('Frontend', '8-999-123-12-22') можно передавать параметры в саму функцию
-// fnLenaInfoLog()
-//2 СПОСОБ Методами call() applay()
 
-// person.logInfo.call(lena, 'Frontend', '8-999-123-12-22') //метод call() вызывает функцию сразу
-
-// person.logInfo.apply(lena, ['Frontend', '8-999-123-12-22']) //метод call() вызывает функцию сразу
-//*****************************
-const array = [1, 2, 3, 4, 5]
-
-// function multBy (arr, n) {
-//   return arr.map(function(i) {
-//     return i*n
-//   })
+// const calc = createCalcFunction(42)
+// calc()
+//
+// function createIncrementor(n) {
+//   return function(num) {
+//     return n + num
+//   }
 // }
-// console.log(multBy(array, 5))
+// const addOne = createIncrementor(1)
+// const addTen = createIncrementor(10)
+//
+// console.log(addOne(10));
+// console.log(addOne(41));
+//
+// console.log(addTen(10));
+// console.log(addTen(41));
 
-// вариант с прототипом.
-Array.prototype.multBy = function(n) {
-  return this.map(function(i) {
-      return i*n
-    })
+function urlGenerator(domain) {
+  return function(url) {
+    return `https://${url}.${domain}`
+  }
 }
 
-console.log(array.multBy(2))
+const comUrl = urlGenerator('com')
+const ruUrl = urlGenerator('ru')
+
+console.log(comUrl('google'))
+console.log(comUrl('netflix'))
+
+console.log(ruUrl('yandex'))
+console.log(ruUrl('vkontakte'))
