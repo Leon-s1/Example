17a9977edd9dfeab603865d7c5770c727152d3e5
leon-s1 <chuprov.sa@gmail.com>
2020-07-16 17:49:45 +0700

    add 5-promise  folder index.js

---
+++ complex-JS/5-promise/index.js
@@ -0,0 +1,51 @@
+console.log('Request data...')
+
+// setTimeout(() => {
+//   console.log('Preparing data...')
+//
+//   const backendData = {
+//     server: 'aws',
+//     port: 2000,
+//     status: 'working'
+//   }
+//
+//   setTimeout(() => {
+//     backendData.modified = true
+//     console.log('Data received', backendData)
+//   }, 2000)
+// }, 2000)
+const p = new Promise(function(resolve, reject) {
+  setTimeout(() => {
+    console.log('Preparing data...')
+    const backendData = {
+      server: 'aws',
+      port: 2000,
+      status: 'working'
+    }
+    resolve(backendData)
+}, 2000)
+})
+
+  p.then((data) => {
+  // console.log('Promise resolved', data)
+  // const p2 = new Promise ((resolve, reject) => {
+  return new Promise ((resolve, reject) => { //вместо переменной можно вернуть промис
+    setTimeout(() => {
+      data.modified = true
+      resolve(data)
+      // console.log('Data received', backendData)
+    }, 2000)
+  })
+    // p2.then(clientData => {
+    //   console.log('Promise resolved', clientData)
+    // })
+  })
+    .then(clientData => {
+    console.log('Promise resolved', clientData)
+    //можно передавать модификации над данными
+      clientData.fromPromise = true
+      return clientData
+  })
+    .then(data => {
+      console.log('Modified', data)
+  })
