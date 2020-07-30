d34a0d14ecc48496f0915d37bb24f15cba25f6ba
Leon-s1 <chuprov.sa@gmail.com>
2020-07-30 23:56:02 +0700

    change index.js in 9-proxy

---
+++ complex-JS/9-proxy/index.js
@@ -0,0 +1,52 @@
+//Проксировать Object
+
+const person = {
+  name: 'Vladilen',
+  age: 26,
+  job: 'Fullstack'
+}
+
+const op = new Proxy(person,{
+  get(target, prop) {
+    console.log(`Getting prop ${prop}`)
+    return target[prop]
+  },
+  set(target, prop, value) {
+    if (prop in target) {
+      target[prop] = value
+    } else {
+      throw new Error(`No ${prop} field in target!`)
+    }
+  },
+  has(target, prop) {
+    return ['age', 'name', 'job'].includes(prop)
+  },
+  deleteProperty(target, prop) {
+    console.log('Deleting...', prop)
+    delete target[prop]
+    return true
+  }
+})
+
+//Functions
+const log = text => `Log: ${text}`
+
+const fp = new Proxy (log, {
+  apply(target, thisArg, args) {   //перехватили вызов функции log методом apply
+    console.log('Calling fn...');
+
+    return target.apply(thisArg,args).toUpperCase()
+  }
+})
+
+//Classes + ключевое слово new
+class Person {
+  constructor(name, age) {
+    this.name = name,
+    this.age = age
+  }
+}
+
+const PersonProxy = new Proxy(Person, {
+  
+}
