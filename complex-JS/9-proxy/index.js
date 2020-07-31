79685e0875261033859bbee270a2935209e96b8b
Leon-s1 <chuprov.sa@gmail.com>
2020-07-31 12:32:11 +0700

    change index.js in 9-proxy

--- complex-JS/9-proxy/index.js
+++ complex-JS/9-proxy/index.js
@@ -48,5 +48,16 @@ class Person {
 }
 
 const PersonProxy = new Proxy(Person, {
-  
-}
+  construct(target, args) {
+    console.log('Construct...')
+
+    return new Proxy (new target(...args), {
+      get(t, prop) {
+        console.log(`Getting prop "${prop}"`)
+        return t[prop]
+      }
+    })
+  }
+})
+
+const p = new PersonProxy('Maxim', 30)
