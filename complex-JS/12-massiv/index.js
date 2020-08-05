5a6c6889e0686b3a926e3ddabae3eb2111098e49
leon-s1 <chuprov.sa@gmail.com>
2020-08-05 17:59:59 +0700

    change 12-massiv folder index.js

--- complex-JS/12-massiv/index.js
+++ complex-JS/12-massiv/index.js
@@ -49,3 +49,72 @@ const people = [
 //то фигурные скобки можно убрать
 
 //Метод Filter
+//реализцация цикла без метода Filter
+//поиск сотрудников возраст > 18
+// const adults = []
+// for (let i = 0; i < people.length; i++) {
+//   if (people[i].age >= 18) {
+//     adults.push(people[i])
+//   }
+// }
+// console.log(adults)
+
+//другая запись при помощи метода Filter
+// const adults = people.filter(person => {
+//   if (person.age >= 18) {
+//     return true
+//   }
+// })
+// console.log(adults)
+//сокращенная запись при помощи метода Filter и стрелочной функции
+// const adults = people.filter(person => person.age >= 18)
+// console.log(adults)
+
+// Reduce
+//реализцация цикла без метода Reduce
+//сумма бюджета всех сотрудников
+// let amount = 0
+// for (let i = 0; i < people.length; i++) {
+//     amount += people[i].budget
+//   }
+// console.log(amount)
+
+//реализцация цикла при помощи метода Reduce
+// const amount = people.reduce((total, person) => {
+//   return total + person.budget
+// }, 0)
+// console.log(amount)
+//упрощенная запись стрелочная функция
+// const amount = people.reduce((total, person) => total + person.budget, 0)
+// console.log(amount)
+
+//Метод Find
+// const igor = people.find(person => person.name === 'Игорь')
+// console.log(igor)
+
+//FindIndex
+// const igorIndex = people.findIndex(person => person.name === 'Игорь')
+// console.log(igorIndex)
+
+//*************************************************
+//практика
+const newPeople = people
+.filter(person => person.budget > 3000)
+.map(person => {
+  return {
+    info: `${person.name} ${person.age}`,
+    budget: person.budget
+  }
+})
+console.log(newPeople);
+// практика посчитать суммарный бюджет при помощи метода reduce
+const amount = people
+.filter(person => person.budget > 3000)
+.map(person => {
+  return {
+    info: `${person.name} ${person.age}`,
+    budget: person.budget
+  }
+})
+.reduce((total, person) => total + person.budget, 0)
+console.log(amount)
