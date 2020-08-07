be27dbdf62ccdbdf315a0d07b1948e85d124b5fd
leon-s1 <chuprov.sa@gmail.com>
2020-08-07 17:08:06 +0700

    change 13-map-set map.js

--- complex-JS/13-map-set/map.js
+++ complex-JS/13-map-set/map.js
@@ -33,5 +33,58 @@ map
   //карты map
 
 // map.delete('job')
-console.log(map.has('job'));
-console.log(map.size);
+// console.log(map.has('job'));
+// console.log(map.size);
+// map.clear()
+// console.log(map.size)
+//***************************************************** */
+// for (let entry of map.entries()) {
+//   console.log(entry);
+// }
+//сокращенная запись
+// for (let [key, value] of map) {
+//   console.log(key, value);
+// }
+// итерация по значениям
+// for (let val of map.values()) {
+//   console.log(val);
+// }
+// итерация по ключам
+// for (let key of map.keys()) {
+//   console.log(key);
+// }
+
+//цикл foreach для map
+// map.forEach((val, key, m) => {
+//   console.log(val, key);
+// })
+//********************************************************* */
+//создание массива из карты разными способами
+// const array = [...map]
+// console.log(array)
+
+// const array = Array.from(map)
+// console.log(array)
+
+// const mapObj = Object.fromEntries(map.entries())
+// console.log(mapObj);
+//***************************************************** */
+
+ const users = [
+   {name: 'Elena'},
+   {name: 'Alex'},
+   {name: 'Irina'}
+ ]
+
+ const visits = new Map()
+
+ visits
+  .set(users[0], new Date())
+  .set(users[1], new Date(new Date().getTime() + 1000 * 60))
+  .set(users[2], new Date(new Date().getTime() + 5000 * 60))
+
+  function lastVisit(user) {
+    return visits.get(user)
+  }
+
+  console.log(lastVisit(users[2]));
