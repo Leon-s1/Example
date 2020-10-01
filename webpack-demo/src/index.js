6565945aa08df801e532e30a533491f34aa37a40
Leon-s1 <chuprov.sa@gmail.com>
2020-10-01 15:21:31 +0700

    change webpack-demo home

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,7 +1,10 @@
+import _ from 'lodash'
+
 function component() {
     const element = document.createElement('div')
 
-    element.innerHTML = _.join(['Hello', 'webpack', ' '])
+     // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 
     return element
 }
