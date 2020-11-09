6c191d68680b5932a5c6e4ac01151efc32259cf9
leon-s1 <chuprov.sa@gmail.com>
2020-11-09 18:01:18 +0700

    change autospas

--- autospas/src/index.js
+++ autospas/src/index.js
@@ -1,7 +1,7 @@
 import Main from './Main'
 // import HTML from './html/main.html'
 import './styles/styles.css'
-import MainLogo from './assets/logo.jpg'
+// import MainLogo from './assets/logo.jpg'
 
 console.log('Index.JS загружен - из файла index.js')
 
--- autospas/webpack.config.js
+++ autospas/webpack.config.js
@@ -1,36 +1,35 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: 'development',
-    entry: './index.js',
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './index.html'
-        }),
-        new CleanWebpackPlugin()        
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|jpg|svg|gif)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.html$/,
-                use: ['html-loader'],
-              }            
-        ]
-    }
+  },
 }
