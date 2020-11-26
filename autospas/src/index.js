0092d0797ee5976ed2836b1d56dfa19dbc4aece7
leon-s1 <chuprov.sa@gmail.com>
2020-11-26 18:12:19 +0700

    change folder autospas adaptiv

--- autospas/src/index.js
+++ autospas/src/index.js
@@ -10,3 +10,23 @@ console.log('Index.JS загружен - из файла index.js')
 Math.E
 
 // console.log('html', HTML)
+
+// menu.onclick =  function myFunction() {
+//   var x = document.getElementById('myTopnav');
+
+//   if (x.className === "topnav"){
+//       x.className += " responsive";
+//   } else{
+//       x.className = "topnav"
+//   }
+// }
+
+menu.onclick = function myFunction() {
+  var x = document.getElementById('myTopnav')
+
+  if (x.className === 'topnav') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'topnav'
+  }
+}
