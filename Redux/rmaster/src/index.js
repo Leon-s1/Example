288bc2b2993e8fb1def62f2f76476f26c74a88fd
leon-s1 <chuprov.sa@gmail.com>
2020-12-04 16:50:03 +0700

    change rmaster

--- Redux/rmaster/src/index.js
+++ Redux/rmaster/src/index.js
@@ -1,11 +1,23 @@
+import { createStore } from './createStore'
 import './styles.css'
+import { rootReducer } from './redux/rootReduser'
 
 const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
+const asyncBtn = document.getElementById('async')
+const themeBtn = document.getElementById('theme')
 
-let state = 0
+const store = createStore(rootReducer, 0) //получаем объект store который умеет взаимодействовать с данными и говорить компоненту, что в нем что ////то изменилось.//
 
-function render() {
-  counter.textContent = state.toString()
-}
+window.store = store
 
-render()
+addBtn.addEventListener('click', () => {})
+
+subBtn.addEventListener('click', () => {})
+
+asyncBtn.addEventListener('click', () => {})
+
+themeBtn.addEventListener('click', () => {
+  // document.body.classList.toggle('dark')
+})
