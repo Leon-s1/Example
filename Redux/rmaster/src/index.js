2e34acbcd64d633dc303368c7f4d2c3ff8b78f87
leon-s1 <chuprov.sa@gmail.com>
2020-12-07 17:59:34 +0700

    add redux

--- Redux/rmaster/src/index.js
+++ Redux/rmaster/src/index.js
@@ -8,13 +8,27 @@ const subBtn = document.getElementById('sub')
 const asyncBtn = document.getElementById('async')
 const themeBtn = document.getElementById('theme')
 
-const store = createStore(rootReducer, 0) //получаем объект store который умеет взаимодействовать с данными и говорить компоненту, что в нем что ////то изменилось.//
+const store = createStore(rootReducer, 0) //получаем объект store который умеет взаимодействовать с данными и говорить компоненту, что в нем что то изменилось.//
 
-window.store = store
+// window.store = store
 
-addBtn.addEventListener('click', () => {})
+addBtn.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' })
+})
 
-subBtn.addEventListener('click', () => {})
+subBtn.addEventListener('click', () => {
+  store.dispatch({ type: 'DECREMENT' })
+})
+
+store.subscribe(() => {
+  //передаем счетчик страницу с увеличенным или уменьшенным значением за счет подписки на событие кнопки
+  // console.log(store.getState())
+  const state = store.getState()
+
+  counter.textContent = state
+})
+
+store.dispatch({ type: 'INIT_APPLICATION' }) //отрисовываем ноль на странице за счет диспатча значения которого нет
 
 asyncBtn.addEventListener('click', () => {})
 
