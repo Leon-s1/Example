01628e798a2e7068a7c07ddd3494e7df21054d55
leon-s1 <chuprov.sa@gmail.com>
2020-12-25 16:53:11 +0700

    add react

--- react-youtube/src/Todo/TodoItem.js
+++ react-youtube/src/Todo/TodoItem.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types' //импортируем библиотеку prop-types в компонент
 
-export default function TodoItem({ todo, index }) {
+function TodoItem({ todo, index }) {
   return (
     <li>
-      <strong>{index + 1} </strong>
-      {todo.title}
+      <span>
+        <input type="checkbox" />
+        {todo.title}
+        <strong>{index + 1} </strong>
+        <button>&times;</button>
+      </span>
     </li>
   )
 }
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  index: PropTypes.number,
+}
+
+export default TodoItem
