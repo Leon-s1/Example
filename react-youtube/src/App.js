14305eea4999757cf63dd342c90c793e207d5b3f
leon-s1 <chuprov.sa@gmail.com>
2020-12-29 18:08:01 +0700

    add react-youtube

--- react-youtube/src/App.js
+++ react-youtube/src/App.js
@@ -2,19 +2,27 @@ import React from 'react'
 import TodoList from './Todo/TodoList'
 
 function App() {
-  let todos = [
+  const [todos, setTodos] = React.useState([
     { id: 1, completed: false, title: 'Купить хлеб' },
     { id: 2, completed: false, title: 'Купить масло' },
     { id: 3, completed: false, title: 'Купить молоко' },
-  ]
+  ]) //передаем в функцию useState начальное состояние, т.е. весь массив todos
+  // let todos = [   первоначальный вариант массив в переменной todos
+  //   { id: 1, completed: false, title: 'Купить хлеб' },
+  //   { id: 2, completed: false, title: 'Купить масло' },
+  //   { id: 3, completed: false, title: 'Купить молоко' },
+  // ]
 
   function toggleTodo(id) {
-    todos = todos.map((todo) => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo
-    })
+    // todos = todos.map((todo) => { было раньше. новый массив присваивался переменной todos
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
   }
 
   return (
