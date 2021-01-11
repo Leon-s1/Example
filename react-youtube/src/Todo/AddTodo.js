97b1e146c5343b285891fd5506217cbe3ed05bbb
leon-s1 <chuprov.sa@gmail.com>
2021-01-11 18:04:22 +0700

    change react-youtube

--- react-youtube/src/Todo/AddTodo.js
+++ react-youtube/src/Todo/AddTodo.js
@@ -1,1 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+function AddTodo({ onCreate }) {
+  const [value, setValue] = useState('')
+
+  function submitHandler(event) {
+    event.preventDefault()
+
+    if (value.trim()) {
+      onCreate(value) //добавляем новый todo в todoList
+      setValue('') //очищаем поле input от введенного текста
+    }
+  }
+
+  return (
+    <form style={{ marginBottom: '1rem' }} onSubmit={submitHandler}>
+      <input value={value} onChange={(event) => setValue(event.target.value)} />
+      <button type="submit">Add todo</button>
+    </form>
+  )
+}
+
+AddTodo.propTypes = {
+  onCreate: PropTypes.func.isRequired,
+}
+
+export default AddTodo
