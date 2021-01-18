2df29089f3982c34eda35a4da891d22beb2365d8
leon-s1 <chuprov.sa@gmail.com>
2021-01-18 18:16:35 +0700

    change my-app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,14 +3,28 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 class Square extends React.Component {
+  //компонент квадрат
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: null,
+    }
+  }
+
   render() {
-    return <button className="square">{/* TODO */}</button>
+    return (
+      <button className="square" onClick={() => this.setState({ value: 'X' })}>
+        {/* {this.props.value}   было*/}
+        {this.state.value}
+      </button>
+    )
   }
 }
 
 class Board extends React.Component {
+  //компонент доска
   renderSquare(i) {
-    return <Square />
+    return <Square value={i} />
   }
 
   render() {
