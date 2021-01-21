67fb7db830ab6031b47e643ea00f9bab7fe8e355
leon-s1 <chuprov.sa@gmail.com>
2021-01-21 18:07:59 +0700

    change my-app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,25 +3,39 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 //компонент квадрат
-class Square extends React.Component {
-  //конструктор удален, т.к. компонент больше не отслеживает состояние
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     value: null,
-  //   }
-  // }
+//по мере написания кода, заменяем класс Square функцией Square
+// class Square extends React.Component {
+//конструктор удален, т.к. компонент больше не отслеживает состояние
+// constructor(props) {
+//   super(props)
+//   this.state = {
+//     value: null,
+//   }
+// }
 
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {/* {this.props.value}   было, показывает событие Click в браузере*/}
-        {/*this.state.value} { Было 2, устанавливает крестик в квадрат*/}
-        {/* 3 вариант передача состояния из компонента Board */}
-        {this.props.value}
+//теперь Square функциональный компонент
+function Square(props) {
+  return (
+    // <button className="square" onClick={() => this.props.onClick()}> строчка относится к классу Square
+
+    {
+      /* {this.props.value}   было, показывает событие Click в браузере*/
+    },
+    {
+      /*this.state.value} { Было 2, устанавливает крестик в квадрат*/
+    },
+    {
+      /* 3 вариант передача состояния из компонента Board */
+    },
+    (
+      // {this.props.value}  строчка относится к классу Square
+
+      // напишем код для функции Square
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     )
-  }
+  )
 }
 
 class Board extends React.Component {
@@ -30,14 +44,22 @@ class Board extends React.Component {
     super(props)
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   //метод который срабатывает при нажатии на квадрат
   handleClick(i) {
     const squares = this.state.squares.slice() //создаем копию массива для хранения нового состояния в компоненте Board
-    squares[i] = 'X' //при клике по компоненту Square рисуем X
-    this.setState({ squares: squares }) //запиываем новое состояние в копию массива squares
+    if (calculateWinner(squares) || squares[i]) {
+      //если кто то выиграл игру или квадрат заполнен фун-ия handleClick возвращается раньше
+      return
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O' //если состояние true при клике по компоненту Square рисуем X, иначе O
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext, //после очередного хода true меняем на false
+    }) //запиcываем новое состояние в копию массива squares
   }
 
   //рисуем компонент доска
@@ -53,7 +75,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X'
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = 'Winner ' + winner
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    }
 
     return (
       <div>
@@ -92,6 +120,27 @@ class Game extends React.Component {
       </div>
     )
   }
+}
+
+//вспомогательная функция определения победителя
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
 }
 
 // ========================================
