0d5678782f3bb942a02ad8a76a48ce1f04552a02
leon-s1 <chuprov.sa@gmail.com>
2021-02-20 16:29:07 +0700

    react-typescript add Navbar

---
+++ react-typescript/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export function Navbar() {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">
+          React + Typescript
+        </a>
+        <ul className="right hide-on-med-and-down">
+          <li>
+            <a href="/">Список дел</a>
+          </li>
+          <li>
+            <a href="/">Информация</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
