fd38ca09ea5811a92c2f29520e7d9a718aa8f1e3
leon-s1 <chuprov.sa@gmail.com>
2021-03-02 18:02:37 +0700

    react-typescript change App.tsx

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -2,13 +2,24 @@ import React, { useState } from 'react'
 import { Navbar } from './components/Navbar'
 import { TodoForm } from './components/TodoForm'
 import { TodoList } from './components/TodoList'
+import { ITodo } from './interfaces'
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState<ITodo[]>([])
 
   const addHandler = (title: string) => {
-    console.log('Add new todo ', title)
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      completed: false,
+    }
+    // setTodos([newTodo, ...todos])
+    setTodos((prev) => [newTodo, ...prev])
   }
+
+  const toggleHandler = (id: number) => {}
+
+  const removeHandler = (id: number) => {}
 
   return (
     <>
