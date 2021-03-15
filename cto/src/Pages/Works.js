d1dcfb719d8986841ea26c5d1277304436973648
leon-s1 <chuprov.sa@gmail.com>
2021-03-15 18:11:05 +0700

    cto change page works.js

--- cto/src/Pages/Works.js
+++ cto/src/Pages/Works.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react'
+import { Container } from 'react-bootstrap'
+import { Row, Col, Image } from 'react-bootstrap'
+import BMW from './Works/img/'
 
 export default class Works extends Component {
   render() {
-    return <div>Works</div>
+    return (
+      <>
+        <Container>
+          <h2 className="text-center m-4">Наши работы</h2>
+
+          <Container>
+            <Row>
+              <Col xs={6} md={4}>
+                <Image src="/" rounded />
+              </Col>
+              <Col xs={6} md={4}>
+                <Image src="holder.js/171x180" roundedCircle />
+              </Col>
+              <Col xs={6} md={4}>
+                <Image src="holder.js/171x180" thumbnail />
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+        <div>Works</div>
+      </>
+    )
   }
 }
---
+++ cto/src/Pages/Works/Works.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Container } from 'react-bootstrap'
+
+export default class Works extends Component {
+  render() {
+    return (
+      <>
+        <Container>
+          <h2 className="text-center m-4">Наши работы</h2>
+        </Container>
+        <div>Works</div>
+      </>
+    )
+  }
+}
