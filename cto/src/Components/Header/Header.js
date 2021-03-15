b37075237ccef7854846beda813db1968859caf0
Leon-s1 <chuprov.sa@gmail.com>
2021-03-15 20:55:52 +0700

    change pages works.js add img

--- cto/src/Components/Header/Header.js
+++ cto/src/Components/Header/Header.js
@@ -15,67 +15,62 @@ import NavbarToggle from 'react-bootstrap/esm/NavbarToggle'
 import logo from './img/audi.png'
 import Home from '../../Pages/Home'
 import Services from '../../Pages/Service/Services'
-import Works from '../../Pages/Works'
+import Works from '../../Pages/Works/Works'
 import Contacts from '../../Pages/Contact/Contacts'
 import Discount from '../../Pages/Discount/Discounts'
 
-export default class Header extends Component {
-  render() {
-    return (
-      <>
-        <Navbar
-          // fixed="top"
-          collapseOnSelect
-          expand="md"
-          bg="dark"
-          variant="dark"
-        >
-          <Container>
-            <a href="/">
-              <h3 style={{ color: 'white', marginRight: '1rem' }}>
-                {' '}
-                АВТОСЕРВИС
-              </h3>
-            </a>
-            <NavbarBrand href="/">
-              <img
-                src={logo}
-                height="110"
-                width="130"
-                className="d-inline-block aligne-top"
-                alt="Logo"
+export default function Header() {
+  return (
+    <>
+      <Navbar
+        // fixed="top"
+        collapseOnSelect
+        expand="md"
+        bg="dark"
+        variant="dark"
+      >
+        <Container>
+          <a href="/">
+            <h3 style={{ color: 'white', marginRight: '1rem' }}> АВТОСЕРВИС</h3>
+          </a>
+          <NavbarBrand href="/">
+            <img
+              src={logo}
+              height="110"
+              width="130"
+              className="d-inline-block aligne-top"
+              alt="Logo"
+            />
+          </NavbarBrand>
+          <NavbarToggle aria-controls="responsive-navbar-nav" />
+          <NavbarCollapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <NavLink href="/">Домой</NavLink>
+              <NavLink href="/services">Услуги</NavLink>
+              <NavLink href="/discount">Акции</NavLink>
+              <NavLink href="/works">Наши работы</NavLink>
+              <NavLink href="/contacts">Контакты</NavLink>
+            </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
               />
-            </NavbarBrand>
-            <NavbarToggle aria-controls="responsive-navbar-nav" />
-            <NavbarCollapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <NavLink href="/">Домой</NavLink>
-                <NavLink href="/services">Услуги</NavLink>
-                <NavLink href="/discount">Акции</NavLink>
-                <NavLink href="/works">Наши работы</NavLink>
-                <NavLink href="/contacts">Контакты</NavLink>
-              </Nav>
-              <Form inline>
-                <FormControl
-                  type="text"
-                  placeholder="Search"
-                  className="mr-sm-2"
-                />
-                <Button variant="outline-info">Search</Button>
-              </Form>
-            </NavbarCollapse>
-          </Container>
-        </Navbar>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/services" component={Services} />
-            <Route exact path="/discount" component={Discount} />
-            <Route exact path="/works" component={Works} />
-            <Route exact path="/contacts" component={Contacts} />
-          </Switch>
-        </Router>
-      </>
-    )
-  }
+              <Button variant="outline-info">Search</Button>
+            </Form>
+          </NavbarCollapse>
+        </Container>
+      </Navbar>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/services" component={Services} />
+          <Route exact path="/discount" component={Discount} />
+          <Route exact path="/works" component={Works} />
+          <Route exact path="/contacts" component={Contacts} />
+        </Switch>
+      </Router>
+    </>
+  )
 }
--- cto/src/Pages/Works/Works.js
+++ cto/src/Pages/Works/Works.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import { Container } from 'react-bootstrap'
+import { Row, Col, Image } from 'react-bootstrap'
+import bmw from './img/BMW.png'
+import audi from './img/audi.png'
+import mercedes from './img/mercedes.png'
+import landcruiser from './img/landcruiser.png'
 
 export default class Works extends Component {
   render() {
@@ -7,6 +12,21 @@ export default class Works extends Component {
       <>
         <Container>
           <h2 className="text-center m-4">Наши работы</h2>
+
+          <Row className="text-center m-4">
+            <Col xs={6} md={4}>
+              <Image src={bmw} roundedCircle />
+            </Col>
+            <Col xs={6} md={4}>
+              <Image src={audi} roundedCircle />
+            </Col>
+            <Col xs={6} md={4}>
+              <Image src={mercedes} roundedCircle />
+            </Col>
+            <Col xs={6} md={4}>
+              <Image src={landcruiser} roundedCircle />
+            </Col>
+          </Row>
         </Container>
         <div>Works</div>
       </>
