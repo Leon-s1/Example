fd94ab50f15ac79718f9b2f78b141227cdc32f3d
inst <chuprov.sa@gmail.com>
2022-03-10 17:29:20 +0700

    add jquery

--- webpack-hello/webpack.config.js
+++ webpack-hello/webpack.config.js
@@ -1,24 +1,41 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const Webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server');
 
 
 module.exports = {
+    context:path.resolve(__dirname,'src'),
     mode: 'development',
-    entry: './src/main.js',
+    entry: {
+
+    }'./src/main.js',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
     },
-    // devServer: {
-    //     contentBase: './dist'
+    devServer: {
+    static: {
+        directory: path.join(__dirname, 'public'),
+    },
+        compress: true,
+        port: 9000,
+    },
+    //     // contentBase: './dist'
+    //     // stats: 'errors-only'
+    //     contentBase: "./",
+    //     historyApiFallback:true,
+    //     inline:true,
+    //     hot:true
     // },
 
-        plugins: [
-            new Webpack.ProvidePlugin({
-                $: 'jquery',
-                jQuery: 'jquery'
-            }),
-        ]
+    plugins: [
+        new Webpack.ProvidePlugin({
+               $: 'jquery',
+               jQuery: 'jquery'
+        }),
+        // new webpack.HotModuleReplacementPlugin()
+    ],
+
     }
 
