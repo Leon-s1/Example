02475713cc3252bdae36a0543c7149cf2eaa8e22
inst <chuprov.sa@gmail.com>
2022-09-28 17:09:53 +0700

    add models/mapping

--- internet shop/server/index.js
+++ internet shop/server/index.js
@@ -1,8 +1,19 @@
 import config from 'dotenv/config'
 import express from 'express'
+import sequelize from "./sequelize";
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(PORT, () => console.log('Сервер запущен на порту', PORT))
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log('Сервер запущен на порту', PORT))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
