b2883ddf6c7e381524e1151d2fb651a6ea80960b
inst <chuprov.sa@gmail.com>
2022-10-17 15:56:13 +0700

    add middleware — authMiddleware и adminMiddleware, edit router

--- internet shop/server/routes/product.js
+++ internet shop/server/routes/product.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import ProductController from '../controllers/Product.js'
+import authMiddleware from '../middleware/authMiddleware.js'
+import adminMiddleware from '../middleware/adminMiddleware.js'
 
 const router = new express.Router()
 
@@ -18,9 +20,18 @@ router.get('/getall/brandId/:brandId([0-9]+)', ProductController.getAll)
 // список всех товаров каталога
 router.get('/getall', ProductController.getAll)
 router.get('/getone/:id([0-9]+)', ProductController.getOne)
-router.post('/create', ProductController.create)
-router.put('/update/:id([0-9]+)', ProductController.update)
-router.delete('/delete/:id([0-9]+)', ProductController.delete)
+
+// router.post('/create', ProductController.create)
+// router.put('/update/:id([0-9]+)', ProductController.update)
+// router.delete('/delete/:id([0-9]+)', ProductController.delete)
+
+// создать товар каталога — нужны права администратора
+router.post('/create', authMiddleware, adminMiddleware, ProductController.create)
+// обновить товар каталога  — нужны права администратора
+router.put('/update/:id([0-9]+)', authMiddleware, adminMiddleware, ProductController.update)
+// удалить товар каталога  — нужны права администратора
+router.delete('/delete/:id([0-9]+)', authMiddleware, adminMiddleware, ProductController.delete)
+
 
 export default router
 
