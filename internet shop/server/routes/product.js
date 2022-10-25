a8d65fc314882bbd90ef8ccf2e27a1aa42b1ee28
inst <chuprov.sa@gmail.com>
2022-10-25 17:14:57 +0700

    add ProductProp.js

--- internet shop/server/routes/product.js
+++ internet shop/server/routes/product.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import ProductController from '../controllers/Product.js'
-import authMiddleware from '../middleware/authMiddleware.js'
-import adminMiddleware from '../middleware/adminMiddleware.js'
+import ProductPropController from '../controllers/ProductProp.js'
+// import authMiddleware from '../middleware/authMiddleware.js'
+// import adminMiddleware from '../middleware/adminMiddleware.js'
 
 const router = new express.Router()
 
@@ -26,11 +27,50 @@ router.get('/getone/:id([0-9]+)', ProductController.getOne)
 // router.delete('/delete/:id([0-9]+)', ProductController.delete)
 
 // создать товар каталога — нужны права администратора
-router.post('/create', authMiddleware, adminMiddleware, ProductController.create)
+router.post('/create',
+    // authMiddleware,
+    // adminMiddleware,
+    ProductController.create)
 // обновить товар каталога  — нужны права администратора
-router.put('/update/:id([0-9]+)', authMiddleware, adminMiddleware, ProductController.update)
+router.put('/update/:id([0-9]+)',
+    // authMiddleware,
+    // adminMiddleware,
+    ProductController.update)
 // удалить товар каталога  — нужны права администратора
-router.delete('/delete/:id([0-9]+)', authMiddleware, adminMiddleware, ProductController.delete)
+router.delete('/delete/:id([0-9]+)',
+    // authMiddleware,
+    // adminMiddleware,
+    ProductController.delete)
+
+/*
+Свойства
+ */
+
+//Список свойств товара
+router.get('/product/:productId([0-9]+)/property/getall', ProductPropController.getAll)
+//одно свойство товара
+router.get('/product/:productId([0-9]+)/property/getone/:id([1-9]+)', ProductPropController.getOne)
+//создать свойство товара
+router.post('/product/:productId([0-9]+)/property/create',
+    // authMiddleware,
+    // adminMiddleware,
+    ProductPropController.create
+)
+
+// обновить свойство товара
+router.put(
+    '/product/:productId([0-9]+)/property/update/:id([0-9]+)',
+    // authMiddleware,
+    // adminMiddleware,
+    ProductPropController.update
+)
+// удалить свойство товара
+router.delete(
+    '/product/:productId([0-9]+)/property/delete/:id([0-9]+)',
+    // authMiddleware,
+    // adminMiddleware,
+    ProductPropController.delete
+)
 
 
 export default router
