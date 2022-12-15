49e301d4bf40113df7bf068cd2d02b2cafabfc0f
inst <chuprov.sa@gmail.com>
2022-12-15 20:57:55 +0700

    edit app.js Route

--- internet shop/client/src/App.js
+++ internet shop/client/src/App.js
@@ -1,11 +1,40 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+//
+// function App() {
+//   return (
+//     <div>
+//       SHOP
+//     </div>
+//   );
+// }
+//
+// export default App;
 
-function App() {
-  return (
-    <div>
-      SHOP
-    </div>
-  );
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Shop from './pages/Shop.js'
+import Product from './pages/Product.js'
+import Basket from './pages/Basket.js'
+import Login from './pages/Login.js'
+import Signup from './pages/Signup.js'
+import Delivery from './pages/Delivery.js'
+import Contacts from './pages/Contacts.js'
+import NotFound from './pages/NotFound.js'
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Shop/>}/>
+                <Route path='/product/:id' element={<Product />} />
+                <Route path='/basket' element={<Basket />} />
+                <Route path='/login' element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/delivery" element={<Delivery />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
