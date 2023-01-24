9f879fce9b9aebfe792780b00e79fbcead46f8b7
inst <chuprov.sa@gmail.com>
2023-01-24 11:38:24 +0700

    edit onClick

--- internet shop/client/src/components/ProductItem.js
+++ internet shop/client/src/components/ProductItem.js
@@ -1,10 +1,12 @@
 import { Card, Col } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 const ProductItem = ({data}) => {
+    const navigate = useNavigate()
     return (
-        <Col xl={3} lg={4} sm={6} className="mt-3" onClick={() => alert('Переход на страницу товара')}>
+        // <Col xl={3} lg={4} sm={6} className="mt-3" onClick={() => alert('Переход на страницу товара')}>
+        <Col xl={3} lg={4} sm={6} className="mt-3" onClick={() => navigate(`/product/${data.id}`)}>
             <Card style={{width: 200, cursor: 'pointer'}}>
-
                 {data.image ? (
                       <Card.Img variant="top" src={process.env.React_APP_IMG_URL + data.image} />
                     ) : (
