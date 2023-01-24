45f89d1763b3f381638b15b9c750440507567336
inst <chuprov.sa@gmail.com>
2023-01-24 11:27:59 +0700

    add publicRoutes product, basket

--- internet shop/client/src/components/AppRouter.js
+++ internet shop/client/src/components/AppRouter.js
@@ -2,6 +2,8 @@ import {Routes, Route} from 'react-router-dom'
 import Shop from '../pages/Shop.js'
 import Login from "../pages/Login.js";
 import Signup from "../pages/Signup.js";
+import Basket from "../pages/Basket.js";
+import Product from "../pages/Product.js";
 import Delivery from "../pages/Delivery.js";
 import Contacts from "../pages/Contacts.js";
 import NotFound from '../pages/NotFound.js'
@@ -10,11 +12,14 @@ import Admin from "../pages/Admin.js";
 import {Component} from "react";
 import {AppContext} from './AppContext.js'
 import {useContext} from "react";
+import { observer } from "mobx-react-lite";
 
 const publicRoutes = [
     {path: '/', Component: Shop},
     {path: '/login', Component: Login},
     {path: '/signup', Component: Signup},
+    {path: '/product/:id', Component: Product},
+    {path: 'basket', Component: Basket},
     {path: '/delivery', Component: Delivery},
     {path: '/contacts', Component: Contacts},
     {path: '*', Component: NotFound},
@@ -28,7 +33,7 @@ const adminRoutes = [
     {path: '/admin', Component: Admin}
 ]
 
-const AppRouter = () => {
+const AppRouter = observer(() => {
     // const isAuth = true
     // const isAdmin = true
     const {user} = useContext(AppContext)
@@ -45,6 +50,6 @@ const AppRouter = () => {
             )}
         </Routes>
     )
-}
+})
 
 export default AppRouter
