868385d26e36fbd22800845c92462b72c95046b5
inst <chuprov.sa@gmail.com>
2023-02-06 10:34:47 +0700

    Добавляем маршруты для просмотр списка заказов и отдельного заказа.

--- internet shop/client/src/components/AppRouter.js
+++ internet shop/client/src/components/AppRouter.js
@@ -1,24 +1,28 @@
 import { Routes, Route } from 'react-router-dom'
 import Shop from '../pages/Shop.js'
-import Login from "../pages/Login.js";
-import Signup from "../pages/Signup.js";
-import Basket from "../pages/Basket.js";
-import Product from "../pages/Product.js";
-import Delivery from "../pages/Delivery.js";
-import Contacts from "../pages/Contacts.js";
+import Login from '../pages/Login.js'
+import Signup from '../pages/Signup.js'
+import Basket from '../pages/Basket.js'
+import Checkout from '../pages/Checkout.js'
+import Product from '../pages/Product.js'
+import Delivery from '../pages/Delivery.js'
+import Contacts from '../pages/Contacts.js'
 import NotFound from '../pages/NotFound.js'
 import User from '../pages/User.js'
-import Admin from "../pages/Admin.js";
-// import { Component } from "react";
+import UserOrders from '../pages/UserOrders.js'
+import UserOrder from '../pages/UserOrder.js'
+import Admin from '../pages/Admin.js'
 import { AppContext } from './AppContext.js'
-import React, { useContext } from "react";
-import { observer } from "mobx-react-lite";
+import { useContext } from 'react'
+import { observer } from 'mobx-react-lite'
+
 
 const publicRoutes = [
     {path: '/', Component: Shop},
     {path: '/login', Component: Login},
     {path: '/signup', Component: Signup},
     {path: '/product/:id', Component: Product},
+    {path: '/checkout', Component: Checkout},
     {path: 'basket', Component: Basket},
     {path: '/delivery', Component: Delivery},
     {path: '/contacts', Component: Contacts},
@@ -27,6 +31,8 @@ const publicRoutes = [
 
 const authRoutes = [
     {path: '/user', Component: User},
+    {path: '/user/orders', Component: UserOrders},
+    {path: '/user/order/:id', Component: UserOrders},
 ]
 
 const adminRoutes = [
