863afa8cfecd78db2ddb178e53772cb41bc7aeb0
inst <chuprov.sa@gmail.com>
2023-02-14 14:23:37 +0700

    Редактируем компонент App

--- internet shop/client/src/App.js
+++ internet shop/client/src/App.js
@@ -10,7 +10,7 @@
 //
 // export default App;
 
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+// import {BrowserRouter, Routes, Route} from "react-router-dom";
 // import Shop from './pages/Shop.js'
 // import Product from './pages/Product.js'
 // import Basket from './pages/Basket.js'
@@ -19,11 +19,11 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 // import Delivery from './pages/Delivery.js'
 // import Contacts from './pages/Contacts.js'
 // import NotFound from './pages/NotFound.js'
-import AppRouter from "./components/AppRouter";
+// import AppRouter from "./components/AppRouter";
 
-const App = () => {
-    return (
-        <BrowserRouter>
+// const App = () => {
+//     return (
+//         <BrowserRouter>
             {/*<Routes>*/}
             {/*    <Route path='/' element={<Shop/>}/>*/}
             {/*    <Route path='/product/:id' element={<Product />} />*/}
@@ -34,9 +34,58 @@ const App = () => {
             {/*    <Route path="/contacts" element={<Contacts />} />*/}
             {/*    /!*<Route path="*" element={<NotFound />} />*!/*/}
             {/*</Routes>*/}
-            <AppRouter/>
+{/*            <AppRouter/>*/}
+{/*        </BrowserRouter>*/}
+{/*    )*/}
+{/*}*/}
+
+{/*export default App*/}
+// Во-первых, данные о пользователе следует получать в компоненте App.
+// Потому что эти данные требуются компоненту NavBar, чтобы показать нужные ссылки
+// — авторизации или личного кабинета. Чтобы получить эти данные —
+// пришлось создавать HOC-компонент CheckAuth, который показывает loader,
+// пока выполняется запрос на сервер. Если данные будут получены в App и
+// сохранены в хранилище — HOC-компонент CheckAuth будет не нужен.
+
+
+import { BrowserRouter } from 'react-router-dom'
+import AppRouter from './components/AppRouter.js'
+import NavBar from './components/NavBar.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { AppContext } from './components/AppContext.js'
+import { check as checkAuth } from './http/userAPI.js'
+import { useState, useContext, useEffect } from 'react'
+import { observer } from 'mobx-react-lite'
+import Loader from './components/Loader.js'
+
+const App = observer(() => {
+    const { user } = useContext(AppContext)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        checkAuth()
+            .then(data => {
+                if (data) {
+                    user.login(data)
+                }
+            })
+            .finally(
+                () => setLoading(false)
+            )
+    }, [])
+
+    // показываем loader, пока получаем с сервера данные пользователя
+    if (loading) {
+        return <Loader />
+    }
+
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <AppRouter />
         </BrowserRouter>
     )
-}
+})
 
 export default App
