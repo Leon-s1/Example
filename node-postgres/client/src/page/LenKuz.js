69f17fd12fbc5058d0f1a20f7bc699e3c94cdaf7
inst <chuprov.sa@gmail.com>
2023-09-14 11:32:58 +0700

    Передача параметра city в функцию getMerchantsLk, для фильтрации сотрудников по городам. Показ данных при нажатии на кнопку Города Ленинск-Кузнецкий, Москва...

--- node-postgres/client/src/page/LenKuz.js
+++ node-postgres/client/src/page/LenKuz.js
@@ -14,7 +14,7 @@ const LenKuz = () => {
         setTimeout(() => {
             getMerchantsLk();
             setLoading(false)
-        }, 2000)
+        }, 1000)
     }, []);
 
     function getMerchantsLk() {
