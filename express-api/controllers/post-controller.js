0349d50c9a3657d739800759c3ed368e1c5e5ed1
inst <chuprov.sa@gmail.com>
2024-06-07 16:41:49 +0700

    Создан метод createPost в файле post-controller.js. Проверен в Postman, работает.

--- express-api/controllers/post-controller.js
+++ express-api/controllers/post-controller.js
@@ -2,7 +2,28 @@ const {prisma} = require('../prisma/prisma-client')
 
 const PostController = {
     createPost: async (req, res) => {
-        res.send('createPost')
+        // res.send('createPost')
+        const {content} = req.body
+
+        const authorId = req.user.userId
+
+        if (!content) {
+            return res.status(400).json({error: 'Все поля обязательны'})
+        }
+
+        try {
+            const post = await prisma.post.create({
+                data: {
+                    content,
+                    authorId
+                }
+            })
+            res.json(post)
+        } catch (error) {
+            console.error('Create post error')
+
+            res.status(500).json({error: 'Internal server error'})
+        }
     },
     getAllPosts: async (req, res) => {
         res.send('getAllPosts')
