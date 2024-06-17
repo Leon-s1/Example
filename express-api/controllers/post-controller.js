b2d9f814b536ea8106d3aacfd17a193bd363523b
inst <chuprov.sa@gmail.com>
2024-06-17 10:56:31 +0700

    Создан метод getPostById в файле post-controller.js. Проверен в Postman, работает.

--- express-api/controllers/post-controller.js
+++ express-api/controllers/post-controller.js
@@ -53,7 +53,38 @@ const PostController = {
         }
     },
     getPostById: async (req, res) => {
-        res.send('getPostById')
+        // res.send('getPostById')
+        const {id} = req.params
+        const userId = req.user.userId
+
+        try {
+            const post = await prisma.post.findUnique({
+                where: {id},
+                include: {
+                    comments: {
+                        include: {
+                            user: true
+                        }
+                    },
+                    likes: true,
+                    author: true
+                }
+            })
+
+            if (!post) {
+                return res.status(404).json({error: 'Пост не найден'})
+            }
+
+            const postWithLikeInfo = {
+                ...post,
+                likedByUser: post.likes.some(like => like.userId === userId)
+            }
+            res.json(postWithLikeInfo)
+        } catch (error) {
+            console.error('Get post by Id error', error)
+
+            res.status(500).json({error: 'Internal server error'})
+        }
     },
     deletePost: async (req, res) => {
         res.send('deletePost')
