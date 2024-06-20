bdd55fde91c015c2543c119191d77ffb7fd8cd62
inst <chuprov.sa@gmail.com>
2024-06-20 11:21:11 +0700

    Создан метод подписки followUser в файле контроллера подписок follow-controller.js. Просетирован в postman. Работает.

--- express-api/controllers/follow-controller.js
+++ express-api/controllers/follow-controller.js
@@ -2,7 +2,41 @@ const {prisma} = require('../prisma/prisma-client')
 
 const FollowController = {
     followUser: async (req, res) => {
-        res.send('followUser')
+        // res.send('followUser')
+        const {followingId} = req.body
+        const userId = req.user.userId
+
+        if (followingId === userId) {
+            return res.status(500).json({error: 'Вы не можете подписаться на самого себя'})
+        }
+
+        try {
+            const existingSubscription = await prisma.follows.findFirst({
+                where: {
+                    AND: [
+                        {followerId: userId},
+                        {followingId}
+                    ]
+                }
+            })
+
+            if (existingSubscription) {
+                return res.status(400).json({error: 'Подписка уже существует'})
+            }
+
+            await prisma.follows.create({
+                data: {
+                    follower: {connect: {id: userId}},
+                    following: {connect: {id: followingId}}
+                }
+            })
+
+            res.status(201).json({message: 'Подписка успешно создана'})
+        } catch (error) {
+            console.error('Follow error', error)
+            return res.status(500).json({error: 'Internal server error'})
+
+        }
     },
     unfollowUser: async (req, res) => {
         res.send('unfollowUser')
