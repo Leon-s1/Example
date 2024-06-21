9abb2464c8ea0f401db6de52ab255f39e334db16
inst <chuprov.sa@gmail.com>
2024-06-21 18:04:28 +0700

    Разработка клиентской части. Подготовка.

--- fullstack app/react-client/src/App.tsx
+++ fullstack app/react-client/src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
     return (
         <div>
             <Button color="primary">
-                Button
+                123Button
             </Button>
         </div>
     )
