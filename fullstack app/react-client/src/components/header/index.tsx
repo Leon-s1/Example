fbdb752b0d43af237cdc30a129ed363cd674e146
inst <chuprov.sa@gmail.com>
2024-07-30 16:44:22 +0700

    Разработка клиентской части. Добавил кнопку выхода в компонент header.tsx.

--- fullstack app/react-client/src/components/header/index.tsx
+++ fullstack app/react-client/src/components/header/index.tsx
@@ -1,11 +1,25 @@
 import React, {useContext} from 'react';
 import {ThemeContext} from "../theme-provider";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/react";
+import {Button, Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/react";
 import {FaRegMoon} from "react-icons/fa";
 import {LuSunMedium} from "react-icons/lu";
+import {useDispatch, useSelector} from "react-redux";
+import {logout, selectIsAuthenticated} from "../../features/user/userSlice";
+import {useNavigate} from "react-router-dom";
+import {CiLogout} from "react-icons/ci";
 
 export const Header = () => {
     const {theme, toggleTheme} = useContext(ThemeContext)
+    const isAuthenticated = useSelector(selectIsAuthenticated)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        dispatch(logout())
+        localStorage.removeItem('token')
+        navigate('/auth')
+    }
+
     return (
         <Navbar>
             <NavbarBrand>
@@ -18,7 +32,17 @@ export const Header = () => {
                     {theme === 'light' ? <FaRegMoon/> : <LuSunMedium/>}
                 </NavbarItem>
                 <NavbarItem>
-
+                    {
+                        isAuthenticated && (
+                            <Button
+                                color='default'
+                                variant='flat'
+                                className='gap-2'
+                                onClick={handleLogout}
+                            >
+                                <CiLogout/> <span>Выйти</span>
+                            </Button>
+                        )}
                 </NavbarItem>
             </NavbarContent>
         </Navbar>
