eb5480b253ba23a9624cdd8f9c98ab11e74a4f86
inst <chuprov.sa@gmail.com>
2024-07-31 17:31:07 +0700

    Разработка клиентской части. Компонент создания поста create-post.tsx

--- fullstack app/react-client/src/pages/posts/index.tsx
+++ fullstack app/react-client/src/pages/posts/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {useGetAllPostsQuery} from "../../app/services/postsApi";
 
 export const Posts = () => {
+    const {data} = useGetAllPostsQuery()
+
     return (
-        <div>
-            Posts
-        </div>
+        <>
+            <div className='mb-10 w-full'>
+                
+            </div>
+        </>
     );
 };
