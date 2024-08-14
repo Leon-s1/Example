5258c2159920f8182c7c79fe81d31b35728dfbbf
inst <chuprov.sa@gmail.com>
2024-08-14 17:19:43 +0700

    Разработка клиентской части. Добавляем кнопку подписаться в компоненте user-profile.

--- fullstack app/react-client/src/features/user/authGuard.tsx
+++ fullstack app/react-client/src/features/user/authGuard.tsx
@@ -1,13 +1,13 @@
-import React, {JSX} from 'react';
-import {useCurrentQuery} from "../../app/services/userApi";
-import {Spinner} from "@nextui-org/react";
+import { Spinner } from "@nextui-org/react"
+import React, { JSX } from "react"
+import { useCurrentQuery } from "../../app/services/userApi"
 
-export const AuthGuard = ({children}: { children: JSX.Element }) => {
-    const {isLoading} = useCurrentQuery()
+export const AuthGuard = ({ children }: { children: JSX.Element }) => {
+  const { isLoading } = useCurrentQuery()
 
-    if (isLoading) {
-        return <Spinner/>
-    }
+  if (isLoading) {
+    return <Spinner />
+  }
 
-    return children
-};
+  return children
+}
--- fullstack app/react-client/src/pages/user-profile/index.tsx
+++ fullstack app/react-client/src/pages/user-profile/index.tsx
@@ -1,90 +1,114 @@
-import React, {useEffect} from 'react';
-import {useParams} from "react-router-dom";
-import {Button, Card, Image, useDisclosure} from "@nextui-org/react";
-import {resetUser, selectCurrent} from "../../features/user/userSlice";
-import {useGetUserByIdQuery, useLazyCurrentQuery, useLazyGetUserByIdQuery} from "../../app/services/userApi";
-import {useFollowUserMutation, useUnfollowUserMutation} from "../../app/services/followApi";
-import {useDispatch, useSelector} from "react-redux";
-import GoBack from "../../components/go-back";
-import {BASE_URL} from "../../constants";
-import {MdOutlinePersonAddAlt1, MdOutlinePersonAddDisabled} from "react-icons/md";
-import {CiEdit} from "react-icons/ci";
-import {formatToClientDate} from "../../utils/format-to client-date";
-import {ProfileInfo} from "../../components/profile-info";
-import {CountInfo} from "../../components/count-info";
+import { Button, Card, Image, useDisclosure } from "@nextui-org/react"
+import React, { useEffect } from "react"
+import { CiEdit } from "react-icons/ci"
+import {
+  MdOutlinePersonAddAlt1,
+  MdOutlinePersonAddDisabled,
+} from "react-icons/md"
+import { useDispatch, useSelector } from "react-redux"
+import { useParams } from "react-router-dom"
+import {
+  useFollowUserMutation,
+  useUnfollowUserMutation,
+} from "../../app/services/followApi"
+import {
+  useGetUserByIdQuery,
+  useLazyCurrentQuery,
+  useLazyGetUserByIdQuery,
+} from "../../app/services/userApi"
+import { CountInfo } from "../../components/count-info"
+import GoBack from "../../components/go-back"
+import { ProfileInfo } from "../../components/profile-info"
+import { BASE_URL } from "../../constants"
+import { resetUser, selectCurrent } from "../../features/user/userSlice"
+import { formatToClientDate } from "../../utils/format-to client-date"
 
 export const UserProfile = () => {
-    const {id} = useParams<{ id: string }>()
-    const {isOpen, onOpen, onClose} = useDisclosure()
-    const currentUser = useSelector(selectCurrent)
-    const {data} = useGetUserByIdQuery(id ?? '')
-    const [followUser] = useFollowUserMutation()
-    const [unFollowUser] = useUnfollowUserMutation()
-    const [triggerGetUserByIdQuery] = useLazyGetUserByIdQuery()
-    const [triggerCurrentQuery] = useLazyCurrentQuery()
+  const { id } = useParams<{ id: string }>()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const currentUser = useSelector(selectCurrent)
+  const { data } = useGetUserByIdQuery(id ?? "")
+  const [followUser] = useFollowUserMutation()
+  const [unFollowUser] = useUnfollowUserMutation()
+  const [triggerGetUserByIdQuery] = useLazyGetUserByIdQuery()
+  const [triggerCurrentQuery] = useLazyCurrentQuery()
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    useEffect(() => () => {
-        dispatch(resetUser())
-    }, [])
+  useEffect(
+    () => () => {
+      dispatch(resetUser())
+    },
+    [],
+  )
+  const handleFollow = async () => {
+    try {
+      if (id) {
+        data?.isFollowing
+          ? await unFollowUser(id).unwrap()
+          : await followUser({ followingId: id }).unwrap()
 
-    if (!data) {
-        return null
-    }
+        await triggerGetUserByIdQuery(id)
 
-    return (
-        <>
-            <GoBack/>
-            <div className="flex items-center gap-4">
-                <Card className='flex flex-col items-center text-center space-y-4 p-5 flex-2'>
-                    <Image
-                        src={`${BASE_URL}${data.avatarUrl}`}
-                        alt={data.name}
-                        width={200}
-                        height={200}
-                        className='border-4 border-white'
-                    />
-                    <div className="flex flex-col text-2xl font-bold gap-4 item-center">
-                        {data.name}
-                        {
-                            currentUser.id !== id ? (
-                                <Button
-                                    color={data.isFollowing ? 'default' : 'primary'}
-                                    variant='flat'
-                                    className='gap-2'
-                                    endContent={
-                                        data.isFollowing ? (
-                                            <MdOutlinePersonAddDisabled/>
-                                        ) : (
-                                            <MdOutlinePersonAddAlt1/>
-                                        )
-                                    }
-                                >
-                                    {data.isFollowing ? 'Отписаться' : 'Подписаться'}
-                                </Button>
-                            ) : (
-                                <Button
-                                    endContent={<CiEdit/>}
-                                >
-                                    Редактировать
-                                </Button>
-                            )
-                        }
-                    </div>
-                </Card>
-                <Card className='flex flex-col space-y-4 p-5 flex-1'>
-                    <ProfileInfo title='Почта' info={data.email}/>
-                    <ProfileInfo title='Местоположение' info={data.location}/>
-                    <ProfileInfo title='Дата рождения' info={formatToClientDate(data.dateOfBirth)}/>
-                    <ProfileInfo title='Обо мне' info={data.bio}/>
+        await triggerCurrentQuery()
+      }
+    } catch (error) {}
+  }
 
-                    <div className="flex gap-2">
-                        <CountInfo count={data.followers.length} title='Подписчики'/>
-                        <CountInfo count={data.following.length} title='Подписки'/>
-                    </div>
-                </Card>
-            </div>
-        </>
-    );
-};
+  if (!data) {
+    return null
+  }
+
+  return (
+    <>
+      <GoBack />
+      <div className="flex items-center gap-4">
+        <Card className="flex flex-col items-center text-center space-y-4 p-5 flex-2">
+          <Image
+            src={`${BASE_URL}${data.avatarUrl}`}
+            alt={data.name}
+            width={200}
+            height={200}
+            className="border-4 border-white"
+          />
+          <div className="flex flex-col text-2xl font-bold gap-4 item-center">
+            {data.name}
+            {currentUser.id !== id ? (
+              <Button
+                color={data.isFollowing ? "default" : "primary"}
+                variant="flat"
+                className="gap-2"
+                onClick={handleFollow}
+                endContent={
+                  data.isFollowing ? (
+                    <MdOutlinePersonAddDisabled />
+                  ) : (
+                    <MdOutlinePersonAddAlt1 />
+                  )
+                }
+              >
+                {data.isFollowing ? "Отписаться" : "Подписаться"}
+              </Button>
+            ) : (
+              <Button endContent={<CiEdit />}>Редактировать</Button>
+            )}
+          </div>
+        </Card>
+        <Card className="flex flex-col space-y-4 p-5 flex-1">
+          <ProfileInfo title="Почта" info={data.email} />
+          <ProfileInfo title="Местоположение" info={data.location} />
+          <ProfileInfo
+            title="Дата рождения"
+            info={formatToClientDate(data.dateOfBirth)}
+          />
+          <ProfileInfo title="Обо мне" info={data.bio} />
+
+          <div className="flex gap-2">
+            <CountInfo count={data.followers.length} title="Подписчики" />
+            <CountInfo count={data.following.length} title="Подписки" />
+          </div>
+        </Card>
+      </div>
+    </>
+  )
+}
