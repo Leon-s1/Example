eda5aa774e7faa520f20a21fa17fa577fb29fcb4
inst <chuprov.sa@gmail.com>
2024-10-16 17:12:17 +0700

    api на клиенте готов.

--- STS-3.0/react-client/src/constants.ts
+++ STS-3.0/react-client/src/constants.ts
@@ -1,4 +1,4 @@
-import * as process from "node:process"
+// import * as process from "node:process"
 
 export const BASE_URL =
   process.env.NODE_ENV === "production" ? "none" : "http://localhost:3000"
