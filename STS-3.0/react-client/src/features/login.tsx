33f74b85a64f95760b4b38461ab3bb1b85c26e6b
inst <chuprov.sa@gmail.com>
2024-11-06 18:07:52 +0700

    Создал компонент input для компонента login

---
+++ STS-3.0/react-client/src/features/login.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { useForm } from "react-hook-form"
+
+type Props = {
+  setSelected: (value: string) => void
+}
+
+const Login = () => {
+  const {} = useForm()
+  return <div></div>
+}
+
+export default Login
