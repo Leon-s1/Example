87891b05a73d9cc3291ea35acf55430c3eb94b32
inst <chuprov.sa@gmail.com>
2024-11-07 17:31:43 +0700

    Создал компонент login

--- STS-3.0/react-client/src/features/login.tsx
+++ STS-3.0/react-client/src/features/login.tsx
@@ -1,13 +1,52 @@
+import { Link } from ""
 import React from "react"
 import { useForm } from "react-hook-form"
+import Input from "../components/input"
+
+type Login = {
+  email: string
+  password: string
+}
 
 type Props = {
   setSelected: (value: string) => void
 }
 
-const Login = () => {
-  const {} = useForm()
-  return <div></div>
+const Login: React.FC<Props> = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<Login>({
+    mode: "onChange",
+    reValidateMode: "onBlur",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+  return (
+    <form className="flex flex-col gap-4">
+      <Input
+        control={control}
+        name="email"
+        label="Email"
+        type="email"
+        required="Обязательное поле"
+      />
+      <Input
+        control={control}
+        name="password"
+        label="Пароль"
+        type="password"
+        required="Обязательное поле"
+      />
+      <p className="text-center text-small">
+        Нет аккаунта{""}
+        <Link size>Зарегистрируйтес</Link>
+      </p>
+    </form>
+  )
 }
 
 export default Login
