a11d4add218b9bcf5721cc270273e639d7ca77e7
inst <chuprovsa@gmail.com>
2025-04-02 10:21:43 +0700

    Commit

--- STS-2.0/client/src/App.js
+++ STS-2.0/client/src/App.js
@@ -1,56 +1,53 @@
-import React, {useEffect, useState} from 'react';
-import '../src/style/style.css';
-import {BrowserRouter} from "react-router-dom";
-import ItemList from "./components/ItemList";
-import {observer} from "mobx-react";
-import AppRouter from "./components/AppRouter";
-import Header from "./components/Header";
-import AdminButtons from "./components/AdminButtons";
-import Footer from "./components/Footer";
-
+import React, { useEffect, useState } from 'react'
+import '../src/style/style.css'
+import { BrowserRouter } from 'react-router-dom'
+import ItemList from './components/ItemList'
+import { observer } from 'mobx-react'
+import AppRouter from './components/AppRouter'
+import Header from './components/Header'
+import AdminButtons from './components/AdminButtons'
+import Footer from './components/Footer'
 
 const App = observer(() => {
-    const [users, setUsers] = useState(false);
-    const [loading, setLoading] = React.useState(true)
+  const [users, setUsers] = useState(false)
+  const [loading, setLoading] = React.useState(true)
 
-    useEffect(() => {
-        setTimeout(() => {
-            getUsers();
-            setLoading(false)
-        }, 0)
-    }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      getUsers()
+      setLoading(false)
+    }, 0)
+  }, [])
 
-    function getUsers() {
-        fetch('http://localhost:3001')
-            .then(response => {
-                return response.text();
-            })
-            .then(data => {
-                setUsers(data);
-            });
-    }
+  function getUsers() {
+    fetch('http://192.168.1.247:3001')
+      .then((response) => {
+        return response.text()
+      })
+      .then((data) => {
+        setUsers(data)
+      })
+  }
 
-    const user = localStorage.getItem('user.isAdmin')
+  const user = localStorage.getItem('user.isAdmin')
 
-    return (
-        <BrowserRouter>
-            <Header/>
-            <div className="main">
-                <div className='list'>
-                    {loading}
-                    <>
-                        <div className="row__card">
-                            <ItemList/>
-                        </div>
-                        {user ? <AdminButtons/> : <></>
-                        }
-                        <AppRouter/>
-                    </>
-                </div>
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className="main">
+        <div className="list">
+          {loading}
+          <>
+            <div className="row__card">
+              <ItemList />
             </div>
-            <Footer/>
-        </BrowserRouter>
-    )
+            {user ? <AdminButtons /> : <></>}
+            <AppRouter />
+          </>
+        </div>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  )
 })
 export default App
-
