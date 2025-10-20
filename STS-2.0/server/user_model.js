8facb6d30798068be6e079163bb68f1b791b8831
inst <chuprovsa@gmail.com>
2025-10-20 17:03:31 +0700

    Создал Dockerfile и docker-compose.yml

--- STS-2.0/server/user_model.js
+++ STS-2.0/server/user_model.js
@@ -1,87 +1,103 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: 'postgres',
-    // user: 'postgres',
-    password: 'Kjubntx12',
-    //database: 'my_database',
-    database: 'STS',
-    host: 'localhost',
-    port: 5432,
+  host: "localhost",
+  user: "postgres",
+  // user: 'postgres',
+  password: "Vbkfhtgf131715",
+  //database: 'my_database',
+  database: "STS",
+  port: 5440,
 });
 
 const getUsers = () => {
-    return new Promise(function (resolve, reject) {
-        pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-            if (error) {
-                reject(error)
-            }
-            resolve(results.rows);
-        })
-    })
-}
+  return new Promise(function (resolve, reject) {
+    pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(results.rows);
+    });
+  });
+};
 
 const getUsersCity = (userCity) => {
-    return new Promise(function (resolve, reject) {
-        const city = userCity
-        pool.query('SELECT * FROM users WHERE city = $1 ORDER BY fio', [city], (error, results) => {
-            if (error) {
-                reject(error)
-            }
-            resolve(results.rows);
-        })
-    })
-}
+  return new Promise(function (resolve, reject) {
+    const city = userCity;
+    pool.query(
+      "SELECT * FROM users WHERE city = $1 ORDER BY fio",
+      [city],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(results.rows);
+      },
+    );
+  });
+};
 
 const getByEmail = (userEmail) => {
-    return new Promise(function (resolve, reject) {
-        const email = userEmail
-        pool.query('SELECT * FROM users WHERE email = $1', [email], (error, results) => {
-            if (error) {
-                reject(error)
-            }
-            resolve(results.rows);
-        })
-    })
-}
+  return new Promise(function (resolve, reject) {
+    const email = userEmail;
+    pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(results.rows);
+      },
+    );
+  });
+};
 
 const createUser = (body) => {
-    return new Promise(function (resolve, reject) {
-        const {fio, position, int_phone, mobile, email, skype, city} = body
-        pool.query('INSERT INTO users (fio, position, int_phone, mobile, email, skype, city) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *', [fio, position, int_phone, mobile, email, skype, city], (error, results) => {
-            if (error) {
-                reject(error)
-            }
-        })
-    })
-}
+  return new Promise(function (resolve, reject) {
+    const { fio, position, int_phone, mobile, email, skype, city } = body;
+    pool.query(
+      "INSERT INTO users (fio, position, int_phone, mobile, email, skype, city) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [fio, position, int_phone, mobile, email, skype, city],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+      },
+    );
+  });
+};
 
 const deleteUser = (body) => {
-    return new Promise(function (resolve, reject) {
-        const {id} = body
-        pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
-            if (error) {
-                reject(error)
-            }
-        })
-    })
-}
+  return new Promise(function (resolve, reject) {
+    const { id } = body;
+    pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
+      if (error) {
+        reject(error);
+      }
+    });
+  });
+};
 
 const updateUser = (body) => {
-    return new Promise(function (resolve, reject) {
-        const {id, fio, position, int_phone, mobile, email, skype, city} = body
-        pool.query('UPDATE users SET fio = $2, position = $3, int_phone = $4, mobile = $5, email = $6, skype = $7, city = $8 WHERE id = $1', [id, fio, position, int_phone, mobile, email, skype, city], (error, results) => {
-            if (error) {
-                reject(error)
-            }
-        })
-    })
-}
+  return new Promise(function (resolve, reject) {
+    const { id, fio, position, int_phone, mobile, email, skype, city } = body;
+    pool.query(
+      "UPDATE users SET fio = $2, position = $3, int_phone = $4, mobile = $5, email = $6, skype = $7, city = $8 WHERE id = $1",
+      [id, fio, position, int_phone, mobile, email, skype, city],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+      },
+    );
+  });
+};
 
 module.exports = {
-    getUsers,
-    getUsersCity,
-    getByEmail,
-    createUser,
-    deleteUser,
-    updateUser,
-}
+  getUsers,
+  getUsersCity,
+  getByEmail,
+  createUser,
+  deleteUser,
+  updateUser,
+};
