f5bce075eef75ba3fd97252bfd8bfdd25abeda0e
inst <chuprovsa@gmail.com>
2026-01-14 15:58:23 +0700

    commit

--- STS-2.0/client/src/components/Footer.js
+++ STS-2.0/client/src/components/Footer.js
@@ -1,43 +1,37 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div className="base-footer">
-            <div className="base-footer__main">
-                <ul className="listcontact">
-                    <h4>ООО «СИБТРАНССЕРВИС» Ленинск-Кузнецкий</h4>
-                    <li className="cont-phone">
-                        Приемная: +7 (38456) 5-31-29, отдел кадров: 5-31-30,
-                        коммерческий отдел: 5-31-31
-                    </li>
-                    <li className="cont-phone">Факс: +7 (38456) 5-31-28</li>
-                    <li className="cont-email">
-                        <a href="mailto:priemnay@sibtranss.ru">
-                            priemnay@sibtranss.ru
-                        </a>
-                    </li>
-                    <li className="cont-adress">
-                        Россия, г. Ленинск-Кузнецкий, ул. Зорина 8б
-                    </li>
-                </ul>
-                <ul className="listcontact">
-                    <h4>ООО «СИБТРАНССЕРВИС» Новокузнецк</h4>
-                    <li className="cont-phone">Приемная: 8(3843)37-36-65</li>
-                    <li className="cont-email">
-                        <a href="mailto:priemnay@sibtranss.ru">
-                            sibtransservis@yandex.ru
-                        </a>
-                    </li>
-                    <li className="cont-adress">
-                        Россия, Г. Новокузнецк, проезд Ферросплавный 15
-                    </li>
-                    
-                </ul>
-                {/*</div>*/}
-            </div>
-
-        </div>
-    );
+  return (
+    <div className="base-footer">
+      <div className="base-footer__main">
+        <ul className="listcontact">
+          <h4>ООО «СИБТРАНССЕРВИС» Ленинск-Кузнецкий</h4>
+          <li className="cont-phone">
+            Приемная: 7-65-31, отдел кадров: 7-65-32, коммерческий отдел:
+            7-65-33 главный бухгалтер: 7-65-41 бухгалтерия: 7-65-42
+          </li>
+          {/*<li className="cont-phone">Факс: +7 (38456) 5-31-28</li>*/}
+          <li className="cont-email">
+            <a href="mailto:priemnay@sibtranss.ru">priemnay@sibtranss.ru</a>
+          </li>
+          <li className="cont-adress">
+            Россия, г. Ленинск-Кузнецкий, ул. Зорина 8б
+          </li>
+        </ul>
+        <ul className="listcontact">
+          <h4>ООО «СИБТРАНССЕРВИС» Новокузнецк</h4>
+          <li className="cont-phone">Приемная: 8(3843)37-36-65</li>
+          <li className="cont-email">
+            <a href="mailto:priemnay@sibtranss.ru">sibtransservis@yandex.ru</a>
+          </li>
+          <li className="cont-adress">
+            Россия, Г. Новокузнецк, проезд Ферросплавный 15
+          </li>
+        </ul>
+        {/*</div>*/}
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
